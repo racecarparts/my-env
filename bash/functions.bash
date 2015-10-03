ab65b10445dd7a91715ea4ae60d3294bfc6edfcf
Tom Wheeler <wheelertom@users.noreply.github.com>
2015-10-03 00:23:46 -0700

    Removing the Linux conditional in the font installation.

--- bash/functions.bash
+++ bash/functions.bash
@@ -27,21 +27,11 @@ function install_fonts {
   find_command="find \"$my_env_fonts_dir\" \( -name '*.[o,t]tf' -or -name '*.pcf.gz' \) -type f -print0"
 
   if [[ `uname` == 'Darwin' ]]; then
-    # MacOS
     font_dir="$HOME/Library/Fonts/my-env"
-  else
-    # Linux
-    font_dir="$HOME/.fonts"
-    mkdir -p $font_dir
   fi
 
   # Copy all fonts to user fonts directory
   eval $find_command | xargs -0 -I % cp "%" "$font_dir/"
-
-  # Reset font cache on Linux
-  if [[ -n `which fc-cache` ]]; then
-    fc-cache -f $font_dir
-  fi
 
   echo "  All fonts installed to $font_dir"
 }
