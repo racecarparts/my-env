4ee15205d2e3d5f48dfb465ea7d878c0be47fb67
Tom Wheeler <wheelertom@users.noreply.github.com>
2016-01-11 09:43:50 -0800

    fixed setComputerName function

--- bash/functions.bash
+++ bash/functions.bash
@@ -94,7 +94,7 @@ function hideFiles() {
 }
 
 function setComputerName() {
-  sudo scutil --set ComputerName $1
-  sudo scutil --set LocalHostName $1
-  sudo scutil --set HostName $1
+  sudo scutil --set ComputerName $1;
+  sudo scutil --set LocalHostName $1;
+  sudo scutil --set HostName $1;
 }
