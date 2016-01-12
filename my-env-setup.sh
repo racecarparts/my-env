765e5611cb45f241a158b0fd025ca3cdd32f1272
Tom Wheeler <wheelertom@users.noreply.github.com>
2016-01-12 10:52:44 -0800

    adding gradle config

--- my-env-setup.sh
+++ my-env-setup.sh
@@ -8,31 +8,33 @@ JAVA_VERSIONS="java java7"
 echo
 echo "* App installations.  This will take a little while."
 echo "*   Installing Homebrew..."
-     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 echo "*   Installing Homebrew Cask..."
-     brew install caskroom/cask/brew-cask
+    brew install caskroom/cask/brew-cask
 echo "*   Setting up the previous version caskroom"
-     brew tap caskroom/versions
+    brew tap caskroom/versions
 echo "*   Installing terminal apps via brew."
-     brew install $BREW_APP_LIST
+    brew install $BREW_APP_LIST
 echo "*   Now we'll install some binary apps: $CASK_APP_LIST"
-     #brew cask install --force --appdir=/Applications $CASK_APP_LIST
-     brew cask install --appdir=/Applications $CASK_APP_LIST
+    #brew cask install --force --appdir=/Applications $CASK_APP_LIST
+    brew cask install --appdir=/Applications $CASK_APP_LIST
 echo "*   Installing Java: $JAVA_VERSIONS"
-     #brew cask install --force $JAVA_VERSIONS
-     brew cask install $JAVA_VERSIONS
+    #brew cask install --force $JAVA_VERSIONS
+    brew cask install $JAVA_VERSIONS
 echo "*   Done."
 
 echo
 echo "Symlink Setup"
 echo "  Create symlink bash_profile."
-     rm -f ~/.bash_profile; ln -s ~/my-env/bash_profile.sh ~/.bash_profile
+    rm -f ~/.bash_profile; ln -s ~/my-env/bash_profile.sh ~/.bash_profile
 echo "  Create symlink .gitconfig."
-     rm -f ~/.gitconfig; ln -s ~/my-env/conf/git/gitconfig ~/.gitconfig
+    rm -f ~/.gitconfig; ln -s ~/my-env/conf/git/gitconfig ~/.gitconfig
 echo "  Create symlink ssh config."
     rm -f ~/.ssh/config; ln -s ~/my-env/conf/ssh/config ~/.ssh/config
 echo "  Create maven repo symlink."
     rm -Rf ~/.m2; ln -s ~/my-env/conf/m2 ~/.m2
+echo "  Create gradle symlink."
+    rm -Rf ~/.gradle; ln -s ~/my-env/conf/gradle ~/.gradle
 echo "  Create Intellij IDEA 'idea.vmoptions' symlink."
     rm -f ~/Library/Preferences/IntelliJIdea13/idea.vmoptions
     ln -s ~/my-env/conf/intellij-idea-14-ce/idea64.vmoptions ~/Library/Preferences/IdeaIC14/idea64.vmoptions;
